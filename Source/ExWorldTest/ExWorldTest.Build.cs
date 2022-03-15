97e5649ebfda0fafd801aa8094980507ad84c663
Hong Gu <macrogu@qq.com>
2022-03-15 10:53:17 +0800

    add StarterContent

--- Source/ExWorldTest/ExWorldTest.Build.cs
+++ Source/ExWorldTest/ExWorldTest.Build.cs
@@ -1,6 +1,10 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System;
+using System.IO;
 using UnrealBuildTool;
+using Tools.DotNETCommon;
+
 
 public class ExWorldTest : ModuleRules
 {
@@ -8,6 +12,39 @@ public class ExWorldTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameplayAbilities", "GameplayTags",	"GameplayTasks", });
+
+		AddIncludePath(ModuleDirectory);
+
+		AddIncludePath(Path.Combine(Path.GetFullPath(Target.RelativeEnginePath), "Source", "Editor", "Blutility", "Private"));
+
 	}
+
+	private void AddIncludePath(string include_path)
+	{
+		if (!Directory.Exists(include_path))
+		{
+			Fail("Unable to locate include path: " + include_path);
+		}
+
+		PublicIncludePaths.Add(include_path);
+		Trace("Added include path: {0}", include_path);
+	}
+
+	private void Trace(string msg)
+	{
+		Log.TraceInformation(Name + ": " + msg);
+	}
+
+	private void Trace(string format, params object[] args)
+	{
+		Trace(string.Format(format, args));
+	}
+
+	private void Fail(string message)
+	{
+		Trace(message);
+		throw new Exception(message);
+	}
+
 }
--- Source/ExWorldTest/ExWorldTestCharacter.cpp
+++ Source/ExWorldTest/ExWorldTestCharacter.cpp
@@ -5,6 +5,7 @@
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
